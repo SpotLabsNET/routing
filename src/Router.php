b7119a29bab926055f759e563132e0e22006500b
Jevon Wright <jevon@jevon.org>
2014-12-13 00:06:35 +1300

    Implementing first actual test and routing logic

--- src/Router.php
+++ src/Router.php
@@ -8,7 +8,13 @@ class Router {
   static $compiled_routes = null;
 
   static function addRoutes($routes) {
+    self::$compiled_routes = null;
     self::$routes = array_merge(self::$routes, $routes);
+  }
+
+  static function resetRoutes() {
+    self::$compiled_routes = null;
+    self::$routes = array();
   }
 
   static function urlFor($module, $arguments) {
@@ -21,8 +27,45 @@ class Router {
     }
   }
 
+  static $_current_compiled_value = null;
+  static $_current_compiled_count = null;
+
   static function compileRoutes() {
-    // TODO
+    $result = array();
+    foreach (self::$routes as $key => $value) {
+      $key = "#" . preg_quote($key, "#") . "#i";
+      self::$_current_compiled_value = $value;
+      self::$_current_compiled_count = 0;
+
+      $key = preg_replace_callback("#\\\\:([a-z]+)#i", function ($matches) {
+        Router::$_current_compiled_count++;
+        Router::$_current_compiled_value = str_replace(":" . $matches[1], "\\" . Router::$_current_compiled_count, Router::$_current_compiled_value);
+        return "([^/]+)";
+      }, $key);
+
+      $result[$key] = Router::$_current_compiled_value;
+    }
+    self::$compiled_routes = $result;
+  }
+
+  /**
+   * Get the PHP file that should be included for the given route.
+   */
+  static function translate($route) {
+    if (self::$compiled_routes === null) {
+      self::compileRoutes();
+    }
+
+    foreach (self::$compiled_routes as $key => $value) {
+      if (preg_match($key, $route, $matches)) {
+        for ($i = 1; $i < count($matches); $i++) {
+          $value = str_replace("\\" . $i, $matches[$i], $value);
+        }
+        return $value;
+      }
+    }
+
+    return $route . ".php";
   }
 
 }
--- test/RoutingTest.php
+++ test/RoutingTest.php
@@ -1,12 +1,15 @@
 <?php
 
+require(__DIR__ . "/../vendor/autoload.php");
+
 class RoutingTest extends PHPUnit_Framework_TestCase {
 
-  function before() {
+  function setUp() {
     \Openclerk\Router::resetRoutes();
     \Openclerk\Router::addRoutes(array(
       "security/login/password" => "login.php",
       "security/login/:key" => "login-:key.php",
+      "security/register/:key" => "register-:key.php?argument=:key",
     ));
   }
 
@@ -15,6 +18,10 @@ class RoutingTest extends PHPUnit_Framework_TestCase {
     $this->assertEquals("login-openid.php", \Openclerk\Router::translate("security/login/openid"));
     $this->assertEquals("default.php", \Openclerk\Router::translate("default"));
     $this->assertEquals("default/default.php", \Openclerk\Router::translate("default/default"));
+  }
+
+  function testMultipleTranslate() {
+    $this->assertEquals("register-openid.php?argument=openid", \Openclerk\Router::translate("security/register/openid"));
   }
 
 }
